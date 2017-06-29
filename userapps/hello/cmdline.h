7c801d86a5bfed448c1e3dc20509917b491e8611
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-06-29 15:38:18 +1000

    Handle different Rumprun configurations

--- userapps/hello/cmdline.h
+++ userapps/hello/cmdline.h
@@ -1,3 +1,3 @@
 #define RUMPCMDLINE "hello world1"
 
-#define RUMPCONFIG "{,,\"net\" :  {,,\"if\":\"wm0\",, \"type\":\"inet\",,\"method\":\"dhcp\",,},,\"cmdline\": \""RUMPCMDLINE"\",,},,"
+#define RUMPCONFIG "{,,\"cmdline\": \""RUMPCMDLINE"\",,},,"
--- userapps/rumpcommon.mk
+++ userapps/rumpcommon.mk
@@ -22,10 +22,12 @@ RRTOOLCHAIN= i486-rumprun-netbsdelf
 endif
 include ${PWD}/rumprun/platform/sel4/rumprunlibs.mk
 
+RR_CONFIG ?= sel4_generic
+
 $(STAGE_DIR)/bin/$(RTARGET)/$(ARCHIVE): $(BAKE_TARGET) $(PROJECT_BASE)/build2/$(SEL4_ARCH)/$(OBJECTBASE)/rumprun.o | $(BAKE_TARGET)_
 	mkdir -p $(STAGE_DIR)/bin/$(RTARGET)
 ifeq ($(RTARGET), sel4)
-	RUMPRUN_BASEDIR=$(dir $(BASEFILE)) rumprun-bake sel4_generic $@ $<
+	RUMPRUN_BASEDIR=$(dir $(BASEFILE)) rumprun-bake $(RR_CONFIG) $@ $<
 else
 	rumprun-bake hw_generic $@ $<
 	cp -a $@ $(PROJECT_BASE)/images/$(ARCHIVE)-hw
